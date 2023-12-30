7f987116c28fbc2a27fc9b949779a3cb2c55ccab
SyunsukeTooyama <syunsuke.t.2021@outlook.jp>
2023-12-31 02:14:14 +0900

    set initial piece

--- products/chess/chess.py
+++ products/chess/chess.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 import numpy as np
+import string
+import math
 
 # make piece class
 class Piece:
@@ -9,7 +11,6 @@ class Piece:
         self.location = location
         self.name = ""
         self.location_number = self.location_tuple()
-        print(self.location_number)
 
     def location_tuple(self):
         column_dict = {"a": 0, "b": 1, "c": 2, "d":3, "e": 4, "f": 5, "g": 6, "h": 7}
@@ -19,13 +20,16 @@ class Piece:
 
     def set_piece_img(self):
         self.piece_img = pygame.image.load(f"pieces/{self.player}_{self.name}.png")
+        self.piece_img = pygame.transform.scale(self.piece_img, (75, 75))
+        if self.player == "black":
+            self.piece_img = pygame.transform.rotate(self.piece_img, 180)
         return self.piece_img
 
     def move_piece(self):
         pass
 
     def draw_piece(self,screen):      
-        screen.blit(self.piece_img,(20,50))
+        screen.blit(self.piece_img,(340.+self.location_number[0]*75,60.+(7-self.location_number[1])*75))
 
 class King(Piece):
     def __init__(self,player,location):
@@ -86,11 +90,45 @@ class Board:
             pygame.draw.line(self.screen,'black',[340+75*(i+1),60],[340+75*(i+1),660],2)
             pygame.draw.line(self.screen,'black',[340,60+75*(i+1)],[940,60+75*(i+1)],2)
 
+class Game:
+    def __init__(self, turn=0):
+        self.turn = turn
+        self.pieces = []
+        self.king = []
+        self.queen = []
+        self.rooks = []
+        self.bishops = []
+        self.kight = []
+        self.pawns = []
+
+    def initialize_game(self):
+        alps = list(string.ascii_lowercase)[0:8]
+        for color in ["black","white"]:
+            if color == "black":
+                color_sign = +1
+            else:
+                color_sign = -1
+
+
+            for alp in alps[0:8]:
+                self.pieces.append(Pawn(color,(f"{alp}{4.5 + 2.5*color_sign}")))
+
+            for column_num in [-1,+1]:
+                self.pieces.append(Knight(color,(f"{alps[int(3.5 + 1.5*column_num)]}{4.5 + 3.5*color_sign}")))
+                self.pieces.append(Bishop(color,(f"{alps[int(3.5 + 2.5*column_num)]}{4.5 + 3.5*color_sign}")))
+                self.pieces.append(Rook(color,(f"{alps[int(3.5 + 3.5*column_num)]}{4.5 + 3.5*color_sign}")))
+
+            self.pieces.append(Queen(color,(f"d{4.5 + 3.5*color_sign}")))
+            self.pieces.append(King(color,(f"e{4.5 + 3.5*color_sign}")))
+
+        print(self.pieces)
+        return self.pieces
+
 def main():
     board = Board()
-    pawn = Pawn("black",("b1"))
-    print(pawn.player)
-
+    pawn = Pawn("black",("c3"))
+    game = Game()
+    pieces = game.initialize_game()
     # draw window
     pygame.init()
     running = True 
@@ -100,7 +138,10 @@ def main():
                 running = False
         
         board.draw_screen()
-        pawn.draw_piece(board.screen)
+
+        for piece in pieces:
+            piece.draw_piece(board.screen)
+        
         pygame.display.update()
     pygame.quit()
 
